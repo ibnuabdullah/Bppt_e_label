4a5d299a8285d5d03480f1cfb9471ef7e49d5565
ibnuabdullah <47619508+ibnuabdullah@users.noreply.github.com>
2020-01-08 09:26:16 +0700

    Initial commit

---
+++ app/src/main/java/com/bppt/e_label/JsonPlaceHolderApi.java
@@ -0,0 +1,16 @@
+package com.bppt.e_label;
+
+import com.bppt.e_label.Model.Post;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("/api/pk/{id}/")
+    Call <Post> getPosts(@Path(value= "id", encoded = true) String id);
+
+    //@GET("/api/pk/{id}/")
+    //Call <Post1> getPosts(@Path(value= "id", encoded = true) String id);
+}
